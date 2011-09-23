77d63550e690502effc5d128a61c38bf14f2c389
bananacakes <slipmatt@nate.com>
2011-09-23 15:47:11 -0300

    patch to linux upstream 2.6.35.14

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1104,7 +1104,7 @@ struct sched_class {
 					 struct task_struct *task);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	void (*moved_group) (struct task_struct *p, int on_rq);
+	void (*task_move_group) (struct task_struct *p, int on_rq);
 	void (*prep_move_group) (struct task_struct *p, int on_rq);
 #endif
 };
