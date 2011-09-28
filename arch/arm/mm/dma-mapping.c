8902a31abf38e7c8be2c2be4d56a6b99e3e6e001
bananacakes <slipmatt@nate.com>
2011-09-28 08:36:25 -0300

    USB host mode

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -381,7 +381,8 @@ EXPORT_SYMBOL(dma_mmap_writecombine);
  */
 void dma_free_coherent(struct device *dev, size_t size, void *cpu_addr, dma_addr_t handle)
 {
-	WARN_ON(irqs_disabled());
+/// HCD_LOCAL_MEM: WARNING: at arch/arm/mm/dma-mapping.c:408 dma_free_coherent
+///	WARN_ON(irqs_disabled());
 
 	if (dma_release_from_coherent(dev, get_order(size), cpu_addr))
 		return;
